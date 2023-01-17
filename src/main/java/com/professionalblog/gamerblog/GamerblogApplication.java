37ec4efeabd436ad6c12a37829f90cf6758922e1
Luan Colombo <luanbelly@gmail.com>
2023-01-17 12:06:58 +0000

    first commit

---
+++ src/main/java/com/professionalblog/gamerblog/GamerblogApplication.java
@@ -0,0 +1,13 @@
+package com.professionalblog.gamerblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamerblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/professionalblog/gamerblog/GamerblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.professionalblog.gamerblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamerblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
