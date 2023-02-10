7c57bb003b8fd20ecd6b3aaecc50545c100f0596
Luan Colombo <luanbelly@gmail.com>
2023-02-10 15:48:58 +0000

    refactor cors 5

--- src/main/java/com/professionalblog/gamerblog/Configs/security/WebSecurity.java
+++ src/main/java/com/professionalblog/gamerblog/Configs/security/WebSecurity.java
@@ -9,9 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 
 @Configuration
 @EnableWebSecurity
@@ -28,21 +25,6 @@ public class WebSecurity {
                 .and()
                 .csrf().disable();
         return http.build();
-    }
-
-    @Bean
-    public WebMvcConfigurer getCorsConfiguration(){
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowCredentials(true)
-                        .maxAge(3600)
-                        .allowedOrigins("localhost:8080")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
-                        .allowedHeaders("*");
-            }
-        };
     }
     @Bean
     public PasswordEncoder passwordEncoder() {
